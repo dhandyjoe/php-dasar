69b5f8e2924358c60be4697d182ec4d83487bc92
dhandyjoe <dhanjoenkp@gmail.com>
2021-02-20 21:50:43 +0700

    update materi

---
+++ IsFunction.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+    Is Function berfungsi untuk mengecek tipe data dari sebuah data.
+*/
+
+$data = 1.2;
+
+var_dump(is_string($data));
+var_dump(is_int($data));
+var_dump(is_bool($data));
+var_dump(is_double($data));
+var_dump(is_float($data));
+var_dump(is_array($data));
